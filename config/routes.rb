360b8d204525a9d804f7854b3f0878cc4153aeae
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-03-14 00:38:38 +0330

    Delete Hello action. Change root from h hello to home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'application#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
-
-  root 'application#hello'
 end
