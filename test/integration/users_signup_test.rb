fe5978efcdb2a00ccf84ab459e6acdc9ad0a0bdd
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-04-26 18:06:13 +0430

    Write a integration test for invalid submission.

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select '#error_explanation'
+    assert_select '.field_with_errors'
+    assert_select 'form[action="/signup"]'
+
+  end
+
+end
