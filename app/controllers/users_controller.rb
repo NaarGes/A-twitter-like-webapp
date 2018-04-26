74e6fd91df95a103c1df3dcbf5d31a18b70ab1d5
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-04-26 16:49:20 +0430

    Show error messages for unsuccessful signups.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
     @user = User.new
     # debugger
   end
+
+  def create
+    @user = User.new(user_params)  # not the final implementation!
+    if @user.save
+      # handle a successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
 end
