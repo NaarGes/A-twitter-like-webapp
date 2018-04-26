eaffbaa0fe687843016984bddcafcfdaa750c7fc
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-04-26 19:06:18 +0430

    Redirect successful signups and write integration test t for it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)  # not the final implementation!
     if @user.save
-      # handle a successful save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
