f0012cd518cb06f9bb1add84110bf16343944cc4
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-05-19 01:32:40 +0430

    Testing the remember me box.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,24 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+
+  # returns true if a test user is logged in
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # log in as a particular user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
