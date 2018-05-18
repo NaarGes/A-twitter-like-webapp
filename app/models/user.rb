c120ad6300a16ed08b62b6bc0170391cb9c4e86b
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-05-18 23:20:17 +0430

    Login with remembering.

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,8 @@ class User < ApplicationRecord
     update_attribute(:rememmber_digest, User.digest(remember_token))
   end
 
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
