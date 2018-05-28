945bd6244816a9d707e362276820e21c72a4ef85
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-05-29 02:42:04 +0430

    Correcting an error; incorrect variable name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   # returns true if the given token matches the digest
   def authenticated?(remember_token)
     return false if rememmber_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(rememmber_digest).is_password?(remember_token)
   end
 
   # forgets a user
