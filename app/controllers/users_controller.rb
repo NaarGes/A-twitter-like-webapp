9b5fe1d2aa7d7aa984ed7b1e1ea05aa7b4f277b6
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-05-31 13:38:29 +0430

    Edit form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,15 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+  def edit
+    @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
 end
