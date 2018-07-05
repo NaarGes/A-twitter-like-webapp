1d3ca241cbc6c2d76fda9b48b885fce4bf9d279f
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-07-05 20:05:01 +0430

    Friendly forwarding. (Redirect users to where they wanted to go before logging in.) q: :Q

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
-
+    # debugger
   end
 
   def create
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
