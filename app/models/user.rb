fc54ed492d801f44092b114519481ed18746853f
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-07-08 21:33:58 +0430

    Account Activation data model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -39,5 +41,18 @@ class User < ApplicationRecord
   def forget
     update_attribute(:rememmber_digest, nil)
   end
+
+  private
+
+    # converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # creates and assings the activation token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180705175439) do
+ActiveRecord::Schema.define(version: 20180708163523) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20180705175439) do
     t.string "password_digest"
     t.string "rememmber_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
