d30faa0b0df23436d04c749289c9dbdd0639586b
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-07-13 17:23:53 +0430

    Resizing image.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,4 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
   storage :file
 
   # Override the directory where uploaded files will be stored.
