b85ace4c923059c9fcaa4eca475f4bca21a716f7
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-07-13 21:47:30 +0430

    The status feed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,8 +79,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # Follows a user.
