233fff71405aead93f49c8da6e6ba09d0c37ebad
Narges Sadeghi <narges.sadeghi73@gmail.com>
2018-07-13 13:52:13 +0430

    A proto-feed.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,9 +4,10 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:sucess] = "Micropost created!"
+      flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
